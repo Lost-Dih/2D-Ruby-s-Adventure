69d9a5b723aec1d1eeae9c8568fd631971b99af1
Diana <dfranciscamagalhaes@gmail.com>
2026-04-02 21:25:40 +0100

    Set up tilemap collision

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -7,24 +7,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    int currentHealth;
     Vector2 move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        currentHealth = maxHealth;
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    move = MoveAction.ReadValue<Vector2>();
-    Debug.Log(move);
-
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
     void FixedUpdate()
     {
-    Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
-    rigidbody2d.MovePosition(position);
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
